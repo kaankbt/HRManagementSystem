9742459394b0431b28d6bae2794b4d578d1bcaaa
Kaan <92556652+kaankbt@users.noreply.github.com>
2022-02-24 02:15:52 +0300

    project created

---
+++ HRManagementSystem/src/main/java/com/kaankubat/HRManagementSystem/HRManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kaankubat.HRManagementSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HRManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HRManagementSystemApplication.class, args);
+	}
+
+}
