3de4d62cdc1d6bbb6de0d5d01797e103131ed2eb
Kaan <170103074@ogr.alanya.edu.tr>
2022-02-24 23:51:34 +0300

    swagger support created

--- HRManagementSystem/src/main/java/com/kaankubat/HRManagementSystem/HRManagementSystemApplication.java
+++ HRManagementSystem/src/main/java/com/kaankubat/HRManagementSystem/HRManagementSystemApplication.java
@@ -2,12 +2,23 @@ package com.kaankubat.HRManagementSystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @SpringBootApplication
 public class HRManagementSystemApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HRManagementSystemApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HRManagementSystemApplication.class, args);
+    }
 
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.example.hrms"))
+                .build();
+    }
 }
